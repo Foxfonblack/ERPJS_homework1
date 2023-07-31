db56ae2bd42ce06a933249bf9d3038f8a7bccfda
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2023-07-31 13:20:00 +0300

    index.html

--- js/button_scr.js
+++ js/button_scr.js
@@ -1,14 +1,20 @@
-const button = document.querySelector(".btn");
+// const button = document.querySelector(".btn"); 
+const form = document.querySelector(".form") //ссылка на форму
+// console.log(form)
+form.addEventListener("submit", onSubmit)//добавляем обработчик событий на форму (тип события submit - отправка формы)
 // console.log(button);
-button.addEventListener("click", onButtonClick);
-function onButtonClick () {
-    document.body.style.backgroundColor=getRandomHexColor()
+// button.addEventListener("click", onButtonClick);
+function onSubmit (event) {
+    // document.body.style.backgroundColor=getRandomHexColor()
     
-    // console.log("click");  
+    console.dir(event); 
+    event.preventDefault()
+    const color = event.currentTarget.elements.input.value;
+    document.body.style.backgroundColor=color
 }
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  }
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215)
+//       .toString(16)
+//       .padStart(6, 0)}`;
+//   }
 //   console.log(getRandomHexColor());
