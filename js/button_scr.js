b809c983828afd3d4a7e0a4e7bf58f3f15f97ce5
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2023-08-01 16:28:17 +0300

    iii333

--- js/button_scr.js
+++ js/button_scr.js
@@ -1,16 +1,32 @@
 // const button = document.querySelector(".btn"); 
-const form = document.querySelector(".form") //ссылка на форму
+// const form = document.querySelector(".form") //ссылка на форму
+const button = document.getElementById("02")
+button.addEventListener("click", onClick)
+// console.log(button)
 // console.log(form)
-form.addEventListener("submit", onSubmit)//добавляем обработчик событий на форму (тип события submit - отправка формы)
+// form.addEventListener("submit", onSubmit)//добавляем обработчик событий на форму (тип события submit - отправка формы)
 // console.log(button);
 // button.addEventListener("click", onButtonClick);
-function onSubmit (event) {
+let i = 0;
+function onClick (event) {
     // document.body.style.backgroundColor=getRandomHexColor()
     
-    console.dir(event); 
-    event.preventDefault()
-    const color = event.currentTarget.elements.input.value;
-    document.body.style.backgroundColor=color
+    // console.dir(event); 
+    // event.preventDefault()
+  
+    const color = document.getElementById("01").value.replaceAll(",")
+    console.log(color)
+    const colorArr = color.split(" ")
+    console.log(colorArr)
+
+    for(const color of colorArr) {
+        i+=1;
+    document.body.style.backgroundColor=colorArr[i]
+  
+    break
+
+    }
+    // event.currentTarget.elements.input.value;
 }
 // function getRandomHexColor() {
 //     return `#${Math.floor(Math.random() * 16777215)
