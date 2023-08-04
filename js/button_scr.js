cee837bdbe9365a79daf74f26247c62d06296380
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2023-08-04 18:35:45 +0300

    3333

--- js/button_scr.js
+++ js/button_scr.js
@@ -1,22 +1,10 @@
-// const button = document.querySelector(".btn"); 
-// const form = document.querySelector(".form") //ссылка на форму
 const button = document.getElementById("02")
 button.addEventListener("click", onClick)
-// console.log(button)
-// console.log(form)
-// form.addEventListener("submit", onSubmit)//добавляем обработчик событий на форму (тип события submit - отправка формы)
-// console.log(button);
-// button.addEventListener("click", onButtonClick);
 let i = 0;
 function onClick (event) {
-    // document.body.style.backgroundColor=getRandomHexColor()
-    
-    // console.dir(event); 
-    // event.preventDefault()
-  
-    const color = document.getElementById("01").value.replaceAll(",", "")
+    const color = document.getElementById("01").value.trim()
     console.log(color)
-    const colorArr = color.split(" ")
+    const colorArr = color.split(",")
     console.log(colorArr)
 
     for(const color of colorArr) {
@@ -27,15 +15,5 @@ function onClick (event) {
         break;
       }
       else i = 0;
- 
-   
-
     }
-    // event.currentTarget.elements.input.value;
 }
-// function getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215)
-//       .toString(16)
-//       .padStart(6, 0)}`;
-//   }
-//   console.log(getRandomHexColor());
