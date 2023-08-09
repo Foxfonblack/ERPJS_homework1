ce71bbcb1f7091120c0b1ba3282ee54e1f5a2c20
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2023-08-09 16:45:39 +0300

    09=-0==0-

--- js/button_scr.js
+++ js/button_scr.js
@@ -1,21 +1,41 @@
 const button = document.getElementById("color_button")
 button.addEventListener("click", onClick)
-let i = 0;
-function onClick (event) {
-    const color = document.getElementById("color_input").value.replaceAll(" ", "")
-    console.log(color)
-    const colorArr = color.split(",").filter(item=> item.length !== 0)
-    console.log(colorArr)
+// let i = 0;
+function onClick () {
+    const color = document.getElementById("color_input").value.trim()
+    const urlRequest = 'https://unpkg.com/color-name-list@10.10.0/dist/colornames.json'
+    let xhr = new XMLHttpRequest()
+    xhr.open('GET', urlRequest)
+    xhr.responseType = 'json'
+    xhr.send()
+    xhr.onload = function(){
+      const responseObj = xhr.response
+      console.log(responseObj)
+      const result = responseObj.find(({name, hex}) =>{
+        return name.toLowerCase() === color.toLowerCase() || hex === color.toLowerCase()
 
-    for(const color of colorArr) {
-      if (i < colorArr.length) {
-        document.body.style.backgroundColor = colorArr[i];
-        console.log(colorArr[i])
-        i += 1;
-        break;
+      })
+      if (result) {
+        document.body.style.backgroundColor = result.hex
       }
-      else i = 0;
+      else {
+        alert('there is no such a color in this array')
+      }
     }
+    // .replaceAll(" ", "")
+    // console.log(color)
+    // const colorArr = color.split(",").filter(item=> item.length !== 0)
+    // console.log(colorArr)
+
+    // for(const color of colorArr) {
+    //   if (i < colorArr.length) {
+    //     document.body.style.backgroundColor = colorArr[i];
+    //     console.log(colorArr[i])
+    //     i += 1;
+    //     break;
+    //   }
+    //   else i = 0;
+    // }
     // yellow  ,  black,,   ,, white  ,, ,,   red
 }
 
