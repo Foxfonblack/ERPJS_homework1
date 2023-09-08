b0eb218a6fd9a670a96fa8774b85905441b0ffb9
Foxfonblack <47613239+Foxfonblack@users.noreply.github.com>
2023-09-08 09:59:59 +0300

    scroll_add

--- js/btn.js
+++ js/btn.js
@@ -1,8 +1,20 @@
 const button = document.getElementById("color_button")
 const colorText = document.querySelector(".colorText");
 const colorHex = document.querySelector(".colorHex")
+const textArea = document.querySelector(".text_area");
+const input = document.getElementById("formGroupExampleInput");
 
 button.addEventListener("click", onClick)
+input.addEventListener("input", onInputChange)
+
+let value = null;
+let intervalId = [];
+function onInputChange(evt) {
+  value = evt.currentTarget.value;
+  console.log(value)
+}
+
+// let i = 0;
 
 const urlRequest = 'https://unpkg.com/color-name-list@10.10.0/dist/colornames.json'
 let xhr = new XMLHttpRequest()
@@ -17,9 +29,19 @@ xhr.onload = function () {
     colorText.innerHTML = nameMarkup;
     colorHex.innerHTML = hexMarkup;
   }
+
+  // console.log(responseObj)
   return responseObj
 }
 xhr.send()
+
+// function onLoad () {
+//    const nameMarkup =  createNameMarkup(xhr.onload());
+//     const hexMarkup = createHexMarkup( xhr.onload());
+//    colorText.innerHTML = nameMarkup;
+//    colorHex.innerHTML = hexMarkup;
+// }
+// onLoad()
 
 function createNameMarkup(arr) {
   const resp = arr.map(({ name }) => `<li class="color">${name}</li>`)
@@ -31,16 +53,48 @@ function createHexMarkup(arr) {
   return resp.join("")
 }
 
+let history = "";
+
+
+
 function onClick() {
+  for (let i = 0; i < intervalId.length; i += 1) {
+    clearInterval(intervalId[i])
+  }
+
   const color = document.querySelector(".form-control").value.trim()
   const response = xhr.onload();
+  console.log(response)
+  const length = value.length;
   const result = response.find(({ name, hex }) => {
     return name.toLowerCase() === color.toLowerCase() || hex === color.toLowerCase()
+  }) || response.filter(({ name }) => {
+    return name.substring(0,length).toLowerCase().includes(value.substring(0,length).toLowerCase())
+    //  name[0].toLowerCase().includes(value)
   })
-  if (result) {
+  console.log(color)
+  console.log(result)
+  if (result.hasOwnProperty("name")) {
     document.body.style.backgroundColor = result.hex
+
+  }
+  else if (result.length) {
+    function showColor(i) {
+     intervalId.push(setTimeout(() => {
+            document.body.style.backgroundColor = result[i].hex
+     }, 1000 * i))
+      
+    }
+    
+    for (let i = 0; i < result.length; i += 1){
+      showColor(i)
+    }
+   
   }
   else {
     alert('there is no such a color in this array')
   }
+  history += " " + document.querySelector(".form-control").value + ",";
+  textArea.value = history;
+  document.querySelector(".form-control").value = "";
 }
